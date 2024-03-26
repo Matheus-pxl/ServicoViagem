50233451d8df88943a7cf6d84705400d00510d80
fiisher <matheushellf@gmail.com>
2024-03-26 10:51:32 -0300

    primeiro commit

---
+++ app/src/main/java/com/projeto/servicoviajem/ui/activity/ListaPacoteActivity.java
@@ -0,0 +1,14 @@
+package com.projeto.servicoviajem.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.projeto.servicoviajem.R;
+
+public class ListaPacoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacote);
+    }
+}
